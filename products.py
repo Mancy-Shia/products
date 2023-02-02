f6f7b974f3a37c2c7abc2690f403e14ab1c4cca8
Mancy <clare99048@gmail.com>
2023-02-02 14:33:05 +0800

    continue

--- products.py
+++ products.py
@@ -2,6 +2,8 @@
 products =[]
 with open('product.csv','r', encoding ='utf-8') as f:
     for line in f:
+        if '商品,價格' in line:
+            continue
         name, price = line.strip().split(',')
         products.append([name,price])
 print(products)
