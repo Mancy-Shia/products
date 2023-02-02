a363bab1b341d163d79d3eb6a970389ffbc9e287
Mancy <clare99048@gmail.com>
2023-02-02 14:30:40 +0800

    read file

--- products.py
+++ products.py
@@ -1,3 +1,11 @@
+#讀取
+products =[]
+with open('product.csv','r', encoding ='utf-8') as f:
+    for line in f:
+        name, price = line.strip().split(',')
+        products.append([name,price])
+print(products)
+#
 products =[]
 while True:
     name = input('Please enter the name of the product:')
@@ -6,9 +14,10 @@ while True:
     price = input('Please enter the price:')
     products.append([name,price])
 print(products)
-
-# for p in products:
-#     print(p)
+#印出商品名稱價格
+for p in products:
+    print(p)
+#寫入檔案
 with open('product.csv','w', encoding ='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
